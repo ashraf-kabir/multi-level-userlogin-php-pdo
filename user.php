02083b5408d2b654fbf7f6c9212496b2d0857b36
Ashraf Kabir <ashrafkabir95@gmail.com>
2019-11-10 14:18:55 +0600

    changes

--- user.php
+++ user.php
@@ -1,22 +1,18 @@
 <?php
 session_start();
-//Checking User Logged or Not
+// checking user logged or not
 if (empty($_SESSION['user'])) {
     header('location: index.php');
 }
-//Restrict admin to access user.php page
+// restrict admin to access user.php page
 if ($_SESSION['user']['role']=='admin') {
     header('location: admin.php');
-}
-//Restrict moderator to access user.php page
-if ($_SESSION['user']['role']=='moderator') {
-    header('location: moderator.php');
 }
 ?>
 <h1>Welcome to <?php echo $_SESSION['user']['username'];?> Page</h1>
 
 <link rel="stylesheet" href="style.css" type="text/css"/>
 <div id="profile">
-    <h2>User name is: <?php echo $_SESSION['user']['username'];?> and Your Role is :<?php echo $_SESSION['user']['role'];?></h2>
+    <h2>username is: <?php echo $_SESSION['user']['username'];?> and Your Role is :<?php echo $_SESSION['user']['role'];?></h2>
 <div id="logout"><a href="logout.php">Please Click To Logout</a></div>
 </div>
